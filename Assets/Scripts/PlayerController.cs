c3de4ece54ee61c3c0713b238c6bde32f4ccb94f
Superwonjune <avgnrolfe@naver.com>
2018-01-19 19:48:55 +0900

    Initial Settings
    프로젝트 설정 화면, 플레이어 배치

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2D;
+
+	void Start () {
+
+        // 컴포넌트 등록
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
