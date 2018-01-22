1414d42f0d8e40be8af6739c7afed1a7ef57fc16
Minsoeng Choi <chanm1996@gmail.com>
2018-01-22 20:47:15 +0900

    아이템 구조 작성 중..

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -8,8 +8,16 @@ public class DestroyByBoundary : MonoBehaviour {
     {
         ObjectsOnGravity objects;
 
+        if (collision.gameObject.CompareTag("Item"))
+            return;
+
         objects = collision.gameObject.GetComponent<ObjectsOnGravity>();
-        if (objects.CompareTag("Bullet"))
+        if (objects == null)
+        {
+            Destroy(collision.gameObject);
+        }
+
+        else if ( objects.CompareTag("White Bullet") || objects.CompareTag("Black Bullet") )
         {
             objects.setDead();
         }
---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour {
+    private Rigidbody2D rb2D;
+    private Collider2D col2D;
+    private SpriteRenderer spriteRenderer;
+
+    public int speed;
+
+    private int itemType;
+    private const int RAPID_FIRE = 1;
+
+	void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        col2D = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //rb2D.velocity = new Vector2(0.0f, -1.0f) * speed;
+        itemType = 0;
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Black Bullet"))
+        {
+            // 검은색 플레이어에게 아이템 제공
+            print("I'm Devil's");
+        }
+        else
+        {
+            // 하얀색 플레이어에게 아이템 제공
+            print("I'm Angel's");
+        }
+    }
+
+    // 누구에게 줄 지, 어떤 아이템인지 판단 필요
+    private void ApplyItem(int playerIndex, int type)
+    {
+
+        // 첫 플레이어 이후는 무시하도록 트리거 설정 해제.
+        col2D.isTrigger = false;
+    }
+
+    public void CreateItem(int type)
+    {
+        itemType = type;
+        
+    }
+
+}
