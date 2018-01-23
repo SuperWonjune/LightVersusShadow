53534acbf004e7f7626e8ebdcb99d43f0000217e
Superwonjune <avgnrolfe@naver.com>
2018-01-23 20:54:31 +0900

    Item sound 추가, GameController 추가

---
+++ Assets/Scripts/SoundEffectManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectManager : MonoBehaviour {
+
+    private AudioSource newAudio;
+
+    void Start () {
+        newAudio = gameObject.GetComponent<AudioSource>();
+    }
+	
+	void Update () {
+		
+	}
+
+
+    public void playSound(string soundFileName)
+    {
+        newAudio.PlayOneShot((AudioClip)Resources.Load(soundFileName));
+    }
+}
